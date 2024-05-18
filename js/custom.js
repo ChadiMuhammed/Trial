c62399adbe30d3415ecaaee4df2e85b19897878e
ChadiMuhammed <61459717+ChadiMuhammed@users.noreply.github.com>
2024-05-18 22:28:29 +0300

    buttons 2
    added the top buttons and enabled the rest of the buttons

--- js/custom.js
+++ js/custom.js
@@ -5,4 +5,26 @@ function getYear() {
     document.querySelector("#displayYear").innerHTML = currentYear;
 }
 
+// Get the button element
+let mybutton = document.getElementById("myBtn");
+
+// Show the button when the user scrolls down
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// Scroll to the top of the document when the button is clicked
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
 getYear();
